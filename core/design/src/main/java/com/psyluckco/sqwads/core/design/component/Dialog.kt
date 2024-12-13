e7f8058c77cff26fb9eb964274853d4f6cc44612
adarshshetty <adarsh.shetty@tismotech.com>
2024-12-13 12:38:05 +0530

    Commits made: - Dialog box buttons added.

--- core/design/src/main/java/com/psyluckco/sqwads/core/design/component/Dialog.kt
+++ core/design/src/main/java/com/psyluckco/sqwads/core/design/component/Dialog.kt
@@ -7,11 +7,16 @@
 package com.psyluckco.sqwads.core.design.component
 
 import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -20,7 +25,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.psyluckco.sqwads.core.design.R
+import com.psyluckco.sqwads.core.design.R.string as AppText
 import com.psyluckco.sqwads.core.design.theme.SqwadsTheme
 
 @Composable
@@ -39,7 +44,7 @@ fun DefaultDialog(
     ) {
 
         Column(
-            modifier = modifier.padding(12.dp)
+            modifier = modifier.padding(24.dp)
         ) {
             Text(
                 text = stringResource(id = title),
@@ -47,9 +52,23 @@ fun DefaultDialog(
                 color = MaterialTheme.colorScheme.primary
             )
 
-            Row {
+            Box(
+                modifier = Modifier.weight(1f)
+            ) {
+            }
+
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = modifier.fillMaxWidth()
+            ) {
                 DefaultTextButton(
-                    text = R.string.placeholder,
+                    modifier = Modifier.width(90.dp),
+                    text = AppText.dismiss_button,
+                    onClick = onDismiss
+                )
+                DefaultTextButton(
+                    modifier = Modifier.width(90.dp),
+                    text = AppText.confirm_button,
                     onClick = onConfirm
                 )
             }
@@ -65,7 +84,7 @@ private fun DefaultDialogPreview() {
 
     SqwadsTheme {
         DefaultDialog(
-            title = R.string.placeholder,
+            title = AppText.placeholder,
             onConfirm = { /*TODO*/ },
             onDismiss = { /*TODO*/ }
         )
