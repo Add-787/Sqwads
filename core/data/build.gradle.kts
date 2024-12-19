c3fe64d5ea82c5a340a550ccb7d9a3f3893cc019
adarshshetty <adarsh.shetty@tismotech.com>
2024-12-19 18:24:53 +0530

    Commits made: - Changed Dialog positioning in HomeScreen.

--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -1,3 +1,5 @@
+import com.psyluckco.sqwads.build_logic.convention.implementation
+
 plugins {
     alias(libs.plugins.sqwads.android.library)
     alias(libs.plugins.sqwads.android.hilt)
@@ -10,6 +12,7 @@ android {
 dependencies {
     implementation(projects.core.model)
     implementation(projects.core.firebase)
+    implementation(libs.timber)
 
     implementation(libs.firebase.auth)
     implementation(project(":core:google"))
